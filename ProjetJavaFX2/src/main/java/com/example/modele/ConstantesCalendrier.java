d3449eea948308d08696e96622514fb8de8ae51b
Daniel Rodrigues Amorim <105808962+Pintass@users.noreply.github.com>
2025-05-05 16:55:05 +0200

    Delete ProjetJavaFX2 directory

--- ProjetJavaFX2/src/main/java/com/example/modele/ConstantesCalendrier.java
+++
@@ -1,7 +0,0 @@
-package com.example.modele;
-
-public interface ConstantesCalendrier {
-    String[] JOURS_SEMAINE = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
-    String[] JOURS_SEMAINE_ABR = {"lu", "ma", "me", "je", "ve", "sa", "di"};
-    String[] MOIS = {"Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"};
-}
--- ProjetJavaFX2/src/main/java/modele/ClientCalendrierDuMois.java
+++
@@ -1,8 +0,0 @@
-package modele;
-
-public class ClientCalendrierDuMois {
-    public static void main(String[] args) {
-        CalendrierDuMois cal = new CalendrierDuMois(5,2004);
-        System.out.println(cal);
-    }
-}
